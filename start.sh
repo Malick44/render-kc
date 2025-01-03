63d1dc7cfbf2c396e49e3021c0288127812d4f13
Malick44 <100083361+Malick44@users.noreply.github.com>
2025-01-03 08:44:17 -0800

    Updated render.yaml to configure database via env vars

--- start.sh
+++ start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Use the provided JDBC URL as is
-DB_JDBC_URL="${DATABASE_URL}"
-DB_USERNAME="${DB_USERNAME}"
-DB_PASSWORD="${DB_PASSWORD}"
-
-# Start Keycloak, using extracted values
+# Start Keycloak, using environment variables
 /opt/keycloak/bin/kc.sh start \
-    --db-url="$DB_JDBC_URL" \
-    --db-username="$DB_USERNAME" \
-    --db-password="$DB_PASSWORD" \
     --hostname="0.0.0.0" \
     --http-relative-path="/auth" \
     --http-port=8080 \
