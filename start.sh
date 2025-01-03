bc9b5ff9dc4a3e4e9e1df3d69b44fff93ac53334
Malick44 <100083361+Malick44@users.noreply.github.com>
2025-01-03 08:49:29 -0800

    Updated start script to enable http and disable https

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Start Keycloak, using environment variables
+# Start Keycloak in HTTP mode
 /opt/keycloak/bin/kc.sh start \
     --hostname="0.0.0.0" \
     --http-relative-path="/auth" \
     --http-port=8080 \
-    --hostname-url="${KEYCLOAK_HOSTNAME_URL}"
+    --hostname-url="${KEYCLOAK_HOSTNAME_URL}"  \
+    --http-enabled=true
