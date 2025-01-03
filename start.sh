2e99e47d15cb47d73c8e3ee73a95b0543cd4cfd4
Malick44 <100083361+Malick44@users.noreply.github.com>
2025-01-03 12:17:02 -0800

    Updated start script to add --hostname-strict=false

--- start.sh
+++ start.sh
@@ -4,10 +4,11 @@ cd /opt/keycloak
 # Start Keycloak in HTTP mode and import realm
 /opt/keycloak/bin/kc.sh start \
     --hostname="0.0.0.0" \
-    # --http-relative-path="/auth" \
+
     --http-port=8080 \
     --http-enabled=true \
     --spi-cluster-jgroups-stack=kubernetes \
     --import-realm --realm-file=/opt/keycloak/realm.json \
     --hostname="${KEYCLOAK_HOSTNAME_URL}" \
     --hostname-strict=false
+ # --http-relative-path="/auth" \
