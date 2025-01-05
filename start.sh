8459ad128040f354d26c4c6350cc353898bf65ab
Malick44 <100083361+Malick44@users.noreply.github.com>
2025-01-05 12:55:06 -0800

    Removed duplicate --hostname parameter from start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 cd /opt/keycloak
 
-# Start Keycloak in HTTP mode
 /opt/keycloak/bin/kc.sh start \
     --optimized \
-    --hostname="${KC_HOSTNAME}" \
-    # --http-relative-path=/auth \
-    --http-port=8080 \
     --http-enabled=true \
-    --proxy=edge \
-    --hostname-strict=false
+    --hostname-strict=false \
+    --hostname="$KC_HOSTNAME"
