0f03e25e74530fac1fef5dbd91bf64f566fc874a
Malick44 <100083361+Malick44@users.noreply.github.com>
2025-01-05 13:08:15 -0800

    Removed duplicate --hostname parameter from start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,10 @@ cd /opt/keycloak
     --optimized \
     --http-enabled=true \
     --hostname-strict=false \
-    --hostname="render-kc-gdz6.onrender.com" \
-    --db=postgres \
+    --db-url-host="render-kc-gdz6.onrender.com" \
+    --db-url-database=postgres \
+    --db-url-port="25256" \
     --db-url="jdbc:postgresql://roundhouse.proxy.rlwy.net:25256/railway" \
     --db-username="postgres" \
     --db-password="DtuPFvzJPWTuGPZBTUCdIePiAXImqxPT"
+
