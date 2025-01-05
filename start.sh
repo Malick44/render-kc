be1ee30e6fce30dfbccc3f0c25c70d21340e3a5a
Malick44 <100083361+Malick44@users.noreply.github.com>
2025-01-05 12:39:10 -0800

    Removed duplicate --hostname parameter from start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,14 @@ cd /opt/keycloak
 
 # Start Keycloak in HTTP mode and import realm
 /opt/keycloak/bin/kc.sh start \
-    -- optimized
-    # --hostname="0.0.0.0" \
+    # -- optimized
+    --hostname="0.0.0.0" \
     --http-relative-path="/auth" \
-    # --http-port=8080 \
+    --db=postgres \
+    --http-port=8080 \
     --http-enabled=true \
+    --health-enabled=true \
     --spi-cluster-jgroups-stack=kubernetes \
     # --import-realm --realm-file=/opt/keycloak/realm.json \
-    # --hostname="${KEYCLOAK_HOSTNAME_URL}" \
+    --hostname="${KEYCLOAK_HOSTNAME_URL}" \
     # --hostname-strict=false
